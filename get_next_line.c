08c5f659deee6358a5bfee5a6c2921019f395c9f
Gabriel hernando Castro sanchez <gcastro-@c1r10s3.42madrid.com>
2022-08-10 19:41:49 +0200

    concepto pillado

--- get_next_line.c
+++ get_next_line.c
@@ -14,12 +14,29 @@
 
 char	*get_next_line(int fd);
 {
-	char	*buff;
-	int		n_bytes;
+	char		*buff;
+	int			n_bytes;		// compare with BUFFER_SIZE
+	static char *stc_var;	// VARIABLE ESTATICA EN LA QUE SE GUARDARÁ EL RESTANTE DE LA LECTURA ANTERIOR. 
+	int			control;
 
-	buff = malloc(20);
-	n_bytes = read(fd, buff, 20);
-	
+	control = 0;
+	buff = malloc(BUFFER_SIZE);
+	stc_var = malloc(BUFFER_SIZE);
+	n_bytes = read(fd, buff, BUFFER_SIZE);
+	while (control == 0)
+	{
+		if(find_LF(buff) == 1)
+		{
+			control = 1;
+			stc = asign_stc(buff);
+		}
+		else
+		{
+			buff = malloc(sizeof(buff) + BUFFER_SIZE);
+			n_bytes = read(fd, &buff[BUFFER_SIZE], BUFFER_SIZE);
+		}
+	}
+
 }
 
 int main()
